a55cb27f1c44e441476546148f06068d392f0cd0
slawhs <gajardo.ignacio@uc.cl>
2024-08-20 13:19:00 -0400

    add JSONQueryEngine

--- json_query_engine/json_retriever.py
+++ json_query_engine/json_retriever.py
@@ -2,12 +2,11 @@
 
 from llama_index.llms.ollama import Ollama
 from llama_index.core.indices.struct_store import JSONQueryEngine
-from llama_index.core import ServiceContext
-from IPython.display import Markdown, display
 from os import path
 import json
 
-with open(path.join('data', 'places.json')) as f:
+
+with open(path.join('data', 'corrected_places.json')) as f:
     json_dict = json.load(f)
 
 with open(path.join('data', 'places_schema.json')) as f:
@@ -15,21 +14,17 @@ with open(path.join('data', 'places_schema.json')) as f:
 
 llm = Ollama(model='llama3', request_timeout=300.0)
 
-def custom_query_processor(query):
-    # Añadir "properties" antes de cualquier clave
-    return query.replace("$.places[", "$.places[?(@.properties.")
-
 # Crear instancia de JSONQueryEngine con la función personalizada
 nl_query_engine = JSONQueryEngine(
     json_value=json_dict,
     json_schema=json_schema,
     llm=llm,
-    query_processor=custom_query_processor  # Asignar la función personalizada
+    verbose=True
 )
 
-
-
-nl_response = nl_query_engine.query("donde queda la sala M1")
-print(nl_response.metadata["json_path_response_str"])
-
-# display(Markdown(nl_response))
+try:
+    query = f"donde queda la sala M1"
+    nl_response = nl_query_engine.query(query)
+    print(nl_response.metadata["json_path_response_str"])
+except ValueError as e:
+    print(f"Error: {e}")
